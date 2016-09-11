b787952aaa82694f89ae41ceaaa6e3917f7edff5
GrinningFerret <luis.hage@outlook.de>
2016-09-11 17:00:50 +0200

    Fix RIL * Switch to caf RIL v11 * Build telephony-ext package
    Change-Id: Ic7a97061a27314de32d26a540bcc433d90ec4804
    Signed-off-by: GrinningFerret <luis.hage@outlook.de>

--- msm8226.mk
+++ msm8226.mk
@@ -160,6 +160,10 @@ PRODUCT_PACKAGES += \
     camera.msm8226 \
     Snap
 
+# Telephony-ext
+PRODUCT_PACKAGES += telephony-ext
+PRODUCT_BOOT_JARS += telephony-ext
+
 ifeq ($(BOARD_HAS_NFC), true)
 # NFC packages
 PRODUCT_PACKAGES += \
